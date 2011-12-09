6321f97481596e485aaf69af8a7ca9ca980a3a4b
ashbb <ashbbb@gmail.com>
2011-12-09 23:32:07 +0900

    Revised layout of the setting window.

--- brains/settings_manager.rb
+++ brains/settings_manager.rb
@@ -14,104 +14,120 @@ class SettingsManager < Shoes::Widget
       def init_ui(win, settings)
         btn_width = (win.width / 2) - 20
         
-        main = flow do
+        main = flow margin_top: 20 do
+          flow margin_left: 10 do
           
-          left = stack displace_left: 10 do
+            left = stack width: btn_width do
             
-            stack top: 20 do
-              para "home music directory:", stroke: gray
-              hmd_btn = button(settings[9]){
-                hmd = ask_open_folder
-                settings[9] = hmd
-              }
-              hmd_btn.style(width: btn_width)
-            end
+              stack do
+                para "home music directory:", stroke: gray
+                hmd_btn = nil
+                flow(width: btn_width){
+                  hmd_btn = para settings[9]
+                }.click{
+                  hmd = ask_open_folder
+                  hmd_btn.text = settings[9] = hmd if hmd
+                }
+              end
             
-            flow displace_top: 25 do
-              para "scale:   ", stroke: gray
-              scale = list_box items: ["100%", "50%", "35%"], choose: settings[4]
-              scale.change{settings[4] = scale.text}
-            end
+              flow(height: 20){}
+              flow do
+                para "scale:   ", stroke: gray
+                scale = list_box items: ["100%", "50%", "35%"], choose: settings[4]
+                scale.change{settings[4] = scale.text}
+              end
             
-            bgc = stack do
-              para "background color:", stroke: gray
-              bg_box = rect(10, 30, btn_width, 25)
-              bg_box.style(fill: settings[8], stroke: gray)
-              bg_box.click{
-                bg_color = ask_color("background")
-                settings[8] = bg_color
-                main.clear
-                init_ui(self, settings)
-              }
-            end
-            bgc.move(0, 160)
-            
-            txtc = stack displace_top: 30 do
-              para "text color:", stroke: gray
-              txtc_box = rect(10, 60, btn_width, 25)
-              txtc_box.style(fill: settings[7], stroke: gray)
-              txtc_box.click{
-                txt_color = ask_color("text")
-                settings[7] = txt_color
-                main.clear
-                init_ui(self, settings)
-              }
-            end
-            
-            txts = flow width: btn_width, displace_top: 75 do
-              @text_sample = para "The quick brown fox jumped over the lazy dog."
-              @text_sample.style(font: settings[5], stroke: settings[7], size: settings[6])
-            end
-          end  #left stack
-          
-          right = stack do
-            
-            para "title format:", stroke: gray, displace_top: 10, stroke: gray
+              flow(height: 20){}
+              bgc = stack do
+                para "background color:", stroke: gray
+                bg_box = flow width: btn_width, height: 25 do
+                  background eval(settings[8])
+                  border gray
+                end
 
-            txt_format = edit_box width: (win.width / 2) - 20
-            txt_format.text = settings[3]
-            txt_format.change{settings[3] = txt_format.text}
+                bg_box.click{
+                  bg_color = ask_color("background")
+                  settings[8] = bg_color.inspect if bg_color
+                  bg_box.clear do
+                    background eval(settings[8])
+                    border gray
+                  end
+                }
+              end
             
-            field = list_box items: ["add field", "#track-number#", "#title#", "#album#", "#artist#", "#album-artist#", "#genre#", "#comments#"], choose: "add field", width: (win.width / 2) - 20
-            field.change{|fld|
-              txt_format.text += fld.text unless fld.text == "add field"
-              fld.choose("add field")
-            }
+              flow(height: 20){}
+              txtc = stack  do
+                para "text color:", stroke: gray
+                txtc_box = flow width: btn_width, height: 25 do
+                  background eval(settings[7])
+                  border gray
+                end
+
+                txtc_box.click{
+                  txtc_color = ask_color("background")
+                  settings[7] = txtc_color.inspect if txtc_color
+                  txtc_box.clear do
+                    background eval(settings[7])
+                    border gray
+                  end
+                }
+              end
             
-            stack displace_top: 20 do
-              para "text font:", stroke: gray
-              fonts = Shoes::FONTS
-              txtfont = list_box items: fonts, choose: settings[5]
-              txtfont.style(width: btn_width)
-              txtfont.change{
-                settings[5] = txtfont.text
-                @text_sample.style(font: settings[5], stroke: settings[7], size: settings[6])
-              }
-            end
+              txts = flow width: btn_width, displace_top: 75 do
+                @text_sample = para "The quick brown fox jumped over the lazy dog.", 
+                  font: settings[5], stroke: eval(settings[7]), size: settings[6].to_i
+              end
+            end  #left stack
           
-            flow displace_top: 45 do
-              para "font size:   ", stroke: gray
-              size = list_box items: ("7".."28").to_a, choose: settings[6]
-              size.change{
-                settings[6] = size.text
-                @text_sample.style(font: settings[5], stroke: settings[7], size: settings[6])
-              }
-            end
-          
-            flow displace_left: 65, displace_top: 90 do
-              button("save settings"){
-                settings << "TITLE_FORMAT"
-                changed
-                notify_observers(settings)
-                self.close
-              }
-              button("cancel"){self.close}
-            end
+            txt_format = nil
+            right = stack width: btn_width do
             
-          end  #right stack
+              para "title format:", stroke: gray, stroke: gray
+
+              txt_format = edit_box width: (win.width / 2) - 20
+              txt_format.text = settings[3]
+            
+              flow(height: 5){}
+              field = list_box items: ["add field", "#track-number#", "#title#", "#album#", "#artist#", "#album-artist#", "#genre#", "#comments#"], choose: "add field", width: (win.width / 2) - 20
+              field.change{|fld| txt_format.text += fld.text unless fld.text == "add field"}
+            
+              flow(height: 20){}
+              stack do
+                para "text font:", stroke: gray
+                fonts = Shoes::FONTS
+                txtfont = list_box items: fonts, choose: settings[5]
+                txtfont.style(width: btn_width)
+                txtfont.change{
+                  settings[5] = txtfont.text
+                  @text_sample.style(font: settings[5], stroke: eval(settings[7]), size: settings[6].to_i)
+                }
+              end
           
-          right.move(((win.width) / 2) + 5, 0)
-        
+              flow(height: 20){}
+              flow do
+                para "font size:   ", stroke: gray
+                size = list_box items: ("7".."28").to_a, choose: settings[6]
+                size.change{
+                  settings[6] = size.text
+                  @text_sample.style(font: settings[5], stroke: eval(settings[7]), size: settings[6].to_i)
+                }
+              end
+          
+              flow(height: 20){}
+              flow do
+                button("save settings"){
+                  settings[3] = txt_format.text
+                  settings << "TITLE_FORMAT"
+                  changed
+                  notify_observers(settings)
+                  self.close
+                }
+                button("cancel"){self.close}
+              end
+            
+            end  #right stack
+          end
+          
         end  #main
         
       end  #init_ui
